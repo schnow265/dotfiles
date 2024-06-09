e2bd5c145b6d281973dd4df0060354214bdf0295
schnow265 <thesnowbox@icloud.com>
2024-06-09 19:06:41 +0000

    my own lil config :)

---
+++ nvim/init.lua
@@ -0,0 +1,31 @@
+-- set keybinds
+vim.g.mapleader = ';'
+
+-- Lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+
+vim.opt.rtp:prepend(lazypath)
+    
+require("lazy").setup("plugins")
+
+vim.cmd 'colorscheme nord'
+
+-- Telescope Keybinds
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+
+-- NeoTree
+vim.api.nvim_set_keymap('n', '<leader>f', ':Neotree filesystem toggle left<CR>', { noremap = true, silent = true })
---
+++ nvim/ts.lua
@@ -0,0 +1,14 @@
+return {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function () 
+      local configs = require("nvim-treesitter.configs")
+
+      configs.setup({
+          ensure_installed = { "c", "lua", "html", "javascript", "typescript" },
+          sync_install = false,
+          highlight = { enable = true },
+          indent = { enable = true },  
+        })
+    end
+ }
