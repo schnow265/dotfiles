84e07445c70b51e8bba960dc30a7daa74efb9a64
schnow265 <thesnowbox@icloud.com>
2024-07-11 19:21:28 +0000

    updated sdkman curl and added an mv line to backup your home.nix if not removed

--- setup.sh
+++ setup.sh
@@ -1,13 +1,13 @@
 # Symlink home-manager config.
-# Please back up your original config file"
-
+# Please back up your original config file. Otherwise the script will move it to a .bak file.
+[ -f ~/.config/home-manager/home.nix ] mv ~/.config/home-manager/home.nix ~/.config/home-manager/home.nix.bak
 ln -s ~/dotfiles/home-manager.nix ~/.config/home-manager/home.nix
 
 # Load doomemacs
-
 git clone --depth 1 https://github.com/doomemacs/doomemacs ~/.config/emacs
 ~/.config/emacs/bin/doom install
-
 rm -rf ~/.emacs ~/.emacs.d
-
 ~/.config/emacs/bin/doom sync
+
+# install sdkman
+curl -s "https://get.sdkman.io?rcupdate=false" | bash
