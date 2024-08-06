f446aee07a6088d295b1aef71bfdde2622ffa58c
schnow265 <thesnowbox@icloud.com>
2024-08-06 17:46:32 +0000

    attempted to use 'spec' like in the docs

--- config/nvim/lua/plugins/whichkey.lua
+++ config/nvim/lua/plugins/whichkey.lua
@@ -7,6 +7,28 @@ return {
   end,
   opts = {
     preset = "helix", 
+    spec = {
+	{ "<leader>f", group = "Telescope" }, -- telescope group
+        { "<leader>ff", desc = "Find Files" },
+        { "<leader>fg", desc = "Live grep" },
+        { "<leader>fb", desc = "Search Buffers" },
+        { "<leader>fh", desc = "Help Tags" },
+
+        { "<leader>x", group = "NeoTree" },
+        { "<leader>xt", desc = "Toggle NeoTree" },
+        { "<leader>xf", desc = "Focus NeoTree" },
+
+        { "<leader>u", group = "Usage Tracker" }, -- Usage Tracker
+
+        { "<leader>l", group = "Git" },
+        { "<leader>lg", desc = "LazyGit" },
+
+        { "<leader>t", group = "Testing" }, -- vim-test
+        { "<leader>tt", desc = "Test nearest" },
+        { "<leader>tl", desc = "Test last" },
+        { "<leader>tf", desc = "Test file" },
+        { "<leader>ts", desc = "Test suite" }
+    },
   },
   keys = {
     {
