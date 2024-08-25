903a0ca13f92f2ae38f9306d09b694bf88b0524a
schnow265 <thesnowbox@icloud.com>
2024-08-25 15:34:54 +0200

    added header image into doom config

--- config/doom/config.el
+++ config/doom/config.el
@@ -74,3 +74,5 @@
 ;;
 ;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
 ;; they are implemented.
+
+(setq fancy-splash-image (concat doom-user-dir "assets/header.png"))
