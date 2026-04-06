51bd7dea15c44a7003b662dbe52a1d4752e0d81a
schnow265 <schnow265@schnow265.dev>
2026-04-06 18:14:22 +0200

    Remove .config/zsh/cfg/08_configs.zsh

--- dot_config/zsh/cfg/08_configs.zsh
+++
@@ -1,6 +0,0 @@
-# Base Directory
-
-CFG_DIR="${XDG_CONFIG_HOME:-${HOME}/.config}"
-
-#[ ! -d $CFG_DIR/nvim ] && git clone https://github.com/schnow265/nvim.conf.git "$CFG_DIR/nvim"
-[ ! -d $CFG_DIR/nvim-oil ] && git clone https://github.com/schnow265/oil.nvim.conf.git "$CFG_DIR/nvim-oil"
