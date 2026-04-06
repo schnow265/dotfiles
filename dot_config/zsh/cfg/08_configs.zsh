30e85e19bb6f3adf74b7f4ec269804fa1252c82a
schnow265 <schnow265@schnow265.dev>
2026-04-06 18:13:06 +0200

    Update .config/zsh/cfg/08_configs.zsh

--- dot_config/zsh/cfg/08_configs.zsh
+++ dot_config/zsh/cfg/08_configs.zsh
@@ -2,5 +2,5 @@
 
 CFG_DIR="${XDG_CONFIG_HOME:-${HOME}/.config}"
 
-[ ! -d $CFG_DIR/nvim ] && git clone https://github.com/schnow265/nvim.conf.git "$CFG_DIR/nvim"
+#[ ! -d $CFG_DIR/nvim ] && git clone https://github.com/schnow265/nvim.conf.git "$CFG_DIR/nvim"
 [ ! -d $CFG_DIR/nvim-oil ] && git clone https://github.com/schnow265/oil.nvim.conf.git "$CFG_DIR/nvim-oil"
