4bdfd52e7f38972e9fa60e439d709132628c9d09
schnow265 <schnow265@schnow265.dev>
2026-04-09 20:32:49 +0200

    Update .config/nvim/lua/config/neovide.lua Update .config/zsh/cfg/09_env.zsh

--- dot_config/nvim/lua/config/neovide.lua
+++ dot_config/nvim/lua/config/neovide.lua
@@ -44,3 +44,5 @@ vim.g.neovide_cursor_animate_in_insert_mode = false
 vim.g.neovide_cursor_animate_command_line = false
 vim.g.neovide_scroll_animation_far_lines = 0
 vim.g.neovide_scroll_animation_length = 0.00
+
+vim.g.neovide_progress_bar_enabled = true
