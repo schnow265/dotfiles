1b794e1c7f35562717c027f1e27d6a354f00726a
schnow265 <schnow265@schnow265.dev>
2026-04-15 20:47:35 +0200

    hardlock blink.cmp

--- dot_config/nvim/lua/kickstart/plugins/blink-cmp.lua
+++ dot_config/nvim/lua/kickstart/plugins/blink-cmp.lua
@@ -5,6 +5,7 @@ return {
 		dependencies = {
 			"folke/lazydev.nvim",
 		},
+		branch = 'v1',
 		build = "cargo build --release",
 		--- @module 'blink.cmp'
 		--- @type blink.cmp.Config
@@ -47,7 +48,7 @@ return {
 			completion = {
 				-- By default, you may press `<c-space>` to show the documentation.
 				-- Optionally, set `auto_show = true` to show the documentation after a delay.
-				documentation = { auto_show = false, auto_show_delay_ms = 500 },
+				documentation = { auto_show = true, auto_show_delay_ms = 500 },
 			},
 
 			sources = {
