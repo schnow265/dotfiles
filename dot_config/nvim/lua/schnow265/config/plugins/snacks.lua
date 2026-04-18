30e80042ac076be28e8c2d98b599c70cd00f8e0a
schnow265 <schnow265@schnow265.dev>
2026-04-18 21:09:33 +0200

    remove some more options

--- dot_config/nvim/lua/schnow265/config/plugins/snacks.lua
+++ dot_config/nvim/lua/schnow265/config/plugins/snacks.lua
@@ -70,9 +70,8 @@ return {
           autokeys = "1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ",
           preset = {
             keys = {
-              { icon = " ", key = "f", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
               { icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
-              { icon = " ", key = "g", desc = "Find Text", action = ":lua Snacks.dashboard.pick('live_grep')" },
+              { icon = " ", key = "f", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
               { icon = "󰒲 ", key = "L", desc = "Lazy", action = ":Lazy", enabled = package.loaded.lazy ~= nil },
               { icon = " ", key = "M", desc = "Mason", action = ":Mason" },
               { icon = " ", key = "q", desc = "Quit", action = ":qa" },
