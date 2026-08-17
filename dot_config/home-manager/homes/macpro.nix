bf039b0b3a2f90e34eddfe73f88724412431aca8
schnow265 <schnow265@schnow265.dev>
2026-08-17 18:11:09 +0200

    format, add and sort

--- dot_config/home-manager/homes/macpro.nix
+++ dot_config/home-manager/homes/macpro.nix
@@ -1,28 +1,39 @@
-{ config, pkgs, scal, ... }:
+{
+  config,
+  pkgs,
+  scal,
+  ...
+}:
 
 let
   binExtract = scal.lib.binExtract { inherit pkgs; };
-in {
+in
+{
   home.username = "scaletto";
   home.homeDirectory = "/Users/scaletto";
 
   home.packages = with pkgs; [
-    cosign
     ffmpeg-full
-
-    bruno
-    mole-cleaner
-    sourcegit
-    vlc-bin
     yt-dlp
+
+    beekeeper-studio
+    bruno
     doomrunner
+    mole-cleaner
     uzdoom
+    vlc-bin
 
     # extracted binarys
     (binExtract android-tools "adb")
+
+    (sourcegit.overrideAttrs (oldAttrs: {
+      runtimeDeps = (oldAttrs.runtimeDeps or [ ]) ++ [
+        git-lfs
+      ];
+    }))
   ];
 
   home.sessionVariables = {
-    CMAKE_INSTALL_PREFIX="/Users/scaletto/.local_builds/";
+    CMAKE_INSTALL_PREFIX = "/Users/scaletto/.local_builds/";
   };
 }
