a47dfe1016fc59f0ba3e46b17b8e1aee8ba13910
schnow265 <schnow265@schnow265.dev>
2026-08-17 19:06:16 +0200

    restructure to add sourcegit as option

--- dot_config/home-manager/apps/sets/git.nix
+++ dot_config/home-manager/apps/sets/git.nix
@@ -1,13 +1,26 @@
-{pkgs, ...}:
 {
-  home.packages = with pkgs; [
-    git
-    delta
-    gh
-    git
-    git-lfs
-    git-xet
-    glab
-    lazygit
-  ];
+  pkgs,
+  enableGitGui ? false,
+  ...
+}:
+{
+  home.packages =
+    with pkgs;
+    [
+      git
+      delta
+      gh
+      git
+      git-lfs
+      git-xet
+      glab
+      lazygit
+    ]
+    ++ lib.optionals enableGitGui [
+      (sourcegit.overrideAttrs (oldAttrs: {
+        runtimeDeps = (oldAttrs.runtimeDeps or [ ]) ++ [
+          git-lfs
+        ];
+      }))
+    ];
 }
