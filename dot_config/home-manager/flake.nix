6255ffa79113fb1a621420a0684334d8045308b2
schnow265 <schnow265@schnow265.dev>
2026-08-21 15:58:07 +0200

    disbale agentic tools

--- dot_config/home-manager/flake.nix
+++ dot_config/home-manager/flake.nix
@@ -64,7 +64,8 @@
           ];
           extraSpecialArgs = {
             enableGuiTools = true;
-            agenticTools = true;
+            agenticTools = false;
+
             inherit masterPkgsDarwin;
           };
         };
