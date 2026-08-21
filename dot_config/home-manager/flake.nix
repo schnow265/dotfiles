241207af9b6b2fc7e5b301a301be8b45424f2a16
schnow265 <schnow265@schnow265.dev>
2026-08-21 15:46:06 +0200

    add the master branch as pkgs input

--- dot_config/home-manager/flake.nix
+++ dot_config/home-manager/flake.nix
@@ -4,6 +4,7 @@
   inputs = {
     # Specify the source of Home Manager and Nixpkgs.
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    nixmaster.url = "github:nixos/nixpkgs";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -16,6 +17,7 @@
   outputs =
     {
       nixpkgs,
+      nixmaster,
       home-manager,
       scal,
       ...
@@ -43,6 +45,9 @@
             inherit username system scal;
           } // extraSpecialArgs;
         };
+
+        masterPkgsDarwin = nixmaster.legacyPackages.aarch64-darwin;
+        masterPkgsLinux = nixmaster.legacyPackages.x86_64-linux;
     in
     {
       homeConfigurations = {
