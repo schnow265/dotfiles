388ec31c65760aac412f27db3bbf85b9491c11ea
schnow265 <schnow265@schnow265.dev>
2026-08-21 15:11:20 +0200

    clean up zshrc

--- dot_config/zsh/cfg/05_evals.zsh
+++ dot_config/zsh/cfg/05_evals.zsh
@@ -2,3 +2,8 @@ zinit as"program" from"gh-r" for starship/starship
 eval "$(starship init zsh)"
 
 [[ -f "$HOME/.cargo/env" ]] && . "$HOME/.cargo/env"
+
+eval "$(direnv hook zsh)"
+
+export SDKMAN_DIR="$HOME/.sdkman"
+[[ -s "$HOME/.sdkman/bin/sdkman-init.sh" ]] && source "$HOME/.sdkman/bin/sdkman-init.sh"
