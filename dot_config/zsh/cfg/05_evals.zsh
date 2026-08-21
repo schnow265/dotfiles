9fbc14854bf352e870e544c5e90e693e4278426e
schnow265 <schnow265@schnow265.dev>
2026-08-21 15:12:19 +0200

    purge sdkman

--- dot_config/zsh/cfg/05_evals.zsh
+++ dot_config/zsh/cfg/05_evals.zsh
@@ -4,6 +4,3 @@ eval "$(starship init zsh)"
 [[ -f "$HOME/.cargo/env" ]] && . "$HOME/.cargo/env"
 
 eval "$(direnv hook zsh)"
-
-export SDKMAN_DIR="$HOME/.sdkman"
-[[ -s "$HOME/.sdkman/bin/sdkman-init.sh" ]] && source "$HOME/.sdkman/bin/sdkman-init.sh"
