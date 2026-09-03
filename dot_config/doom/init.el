538575a5e60fa30e382f336e3c526aecf817ab5d
schnow265 <schnow265@schnow265.dev>
2026-09-03 20:15:52 +0200

    doom emacs: disable nix

--- dot_config/doom/init.el
+++ dot_config/doom/init.el
@@ -156,7 +156,7 @@
        lua               ; one-based indices? one-based indices
        markdown          ; writing docs for people to ignore
        ;;nim               ; python + lisp at the speed of c
-       nix               ; I hereby declare "nix geht mehr!"
+       ;;nix               ; I hereby declare "nix geht mehr!"
        ;;ocaml             ; an objective camel
        ;;odin              ; C, minus its footguns
        org               ; organize your plain life in plain text
