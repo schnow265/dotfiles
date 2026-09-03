d61ca5a9190d828bd79997c99b99966e56d116b7
schnow265 <schnow265@schnow265.dev>
2026-09-03 18:47:08 +0200

    emacs: add spacemacs and set as default

--- dot_config/chemacs/profiles.el
+++ dot_config/chemacs/profiles.el
@@ -1,3 +1,4 @@
 (
+  ("spacemacs" . ((user-emacs-directory . "~/.local/share/external/emacs-cfg/spacemacs")))
   ("doom"      . ((user-emacs-directory . "~/.local/share/external/emacs-cfg/doom")))
 )
